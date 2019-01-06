b8b7aef89bda39fabd033f05a397da866a266e70
Emad Mokhtar <emad.m.habib@gmail.com>
2019-01-06 09:49:56 +0100

    Add test, use vcr to record the API calls, and remove the unused tests

--- tests/test_tafseer_services.py
+++ tests/test_tafseer_services.py
@@ -2,7 +2,16 @@ from tafseer_package.services import QuranTafseer
 import pytest
 
 
+@pytest.mark.vcr()
 def test_get_tafseer_books():
     tafseer_list = QuranTafseer.get_tafseer_books()
     assert tafseer_list is not None
-    assert list(tafseer_list[0].keys()) == ['id', 'name', 'language', 'author', 'book_name']
+    assert list(tafseer_list[0].keys()) == ['id', 'name', 'language', 'author',
+                                            'book_name']
+
+
+@pytest.mark.vcr()
+def test_get_tafseer_books_with_lang():
+    tafseer_list = QuranTafseer.get_tafseer_books()
+    tafseer_list_with_lang = QuranTafseer.get_tafseer_books(language='ar')
+    assert len(tafseer_list) > len(tafseer_list_with_lang)
