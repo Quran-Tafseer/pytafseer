796528cbe69f8e6c2e401f25b2653c4ebe935469
Emad Mokhtar <emad.m.habib@gmail.com>
2019-01-06 09:48:55 +0100

    Implmenet filter by language and extract the URL to settings file

--- tafseer_package/services.py
+++ tafseer_package/services.py
@@ -1,19 +1,30 @@
-import requests
 from urllib.parse import urljoin
 
-class QuranTafseer:
-    web_api_url = 'http://api.quran-tafseer.com/'
+import requests
 
+from .settings import WEB_API_URL
+
+
+class QuranTafseer:
     @classmethod
-    def get_tafseer_books(self) -> dict:
-        """
-        Gets the list of avalaible tafseer
-        :returns: List of dictionary contains list of tafseer book
-        :raises ValueError: raise Value error if the JSON return form the services is invalid.
+    def get_tafseer_books(self, language='') -> list:
+        """get_tafseer_books Gets the list of avalaible tafseer
+
+        :param language: filter the list of tafseer based on lanugage,
+        defaults to ''
+        :param language: str, optional
+        :raises ValueError: raise Value error if the JSON return form the
+        services is invalid.
         :raises Timeout: if the server didn't return any response.
         :raises HTTPError: if the server returned unsuccessful resposne.
+        :return: list of dictonart with tafseer attbutes ['id', 'name',
+        'language', 'author', 'book_name']
+        :rtype: list
         """
-        request_url = urljoin(self.web_api_url, 'tafseer')
-        response = requests.get(request_url)
+        params = {}
+        if language:
+            params['lang'] = language
+        request_url = urljoin(WEB_API_URL, 'tafseer')
+        response = requests.get(request_url, params=params)
         response.raise_for_status()
         return response.json()
